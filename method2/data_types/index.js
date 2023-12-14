795dcbdf8e7d6ffd79969ca92ce11a398ce18f4e
sachin2uinstructor <sjhaveri@instructors.2u.com>
2023-12-14 14:53:44 -0500

    Primitive Data Types

---
+++ method2/data_types/index.js
@@ -0,0 +1,28 @@
+console.log("Hello from external JS file");
+
+// Different data types - 
+
+// 1. Primitive
+// String, Number, Boolean, Undefined, Null, BigInt, Symbol
+
+// 2. Non Primitive
+// Objects
+
+const name = 'Alex'
+const language = 'English'
+const channel = 'codevolution'
+
+// Number
+const total = 0
+const PI = 3.14
+
+// Boolean
+const isPrimary = true
+const isNewUser = false
+
+// Undefined
+let result;
+const res = undefined;
+
+// Null
+const data = null
