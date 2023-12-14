df8e2f3df1418553aa6b05c43b0766af728f640d
sachin2uinstructor <sjhaveri@instructors.2u.com>
2023-12-14 14:48:33 -0500

    variables annd types of variables in JS

---
+++ method2/variables/index.js
@@ -0,0 +1,15 @@
+console.log("Hello from external JS file");
+
+// Variables
+
+// var
+var uname = "Jake"
+console.log(uname);
+
+// let
+let age = 25;
+console.log(age);
+
+// const
+const salary = 80000;
+console.log(salary);
