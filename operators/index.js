e665e08d4ad9cadc8b467ee821ba7668e7a4d3e0
sachin2uinstructor <sjhaveri@instructors.2u.com>
2023-12-14 15:04:36 -0500

    Assignment operators and operations

---
+++ operators/index.js
@@ -0,0 +1,26 @@
+console.log("Hello from external JS file");
+
+// Variables
+
+// var
+var uname = "Jake"
+console.log(uname);
+
+// let
+let age = 25;
+console.log(age);
+
+// const
+const salary = 80000;
+console.log(salary);
+
+// Assignment operator
+
+let x=10;
+let y=5;
+
+console.log(x + y); // Add
+console.log(x * y); // Multiply
+console.log(x - y); // Subtract
+console.log(x / y); // Divide
+console.log(x % y); // Mod
